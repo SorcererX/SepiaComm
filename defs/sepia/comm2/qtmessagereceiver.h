e9f6c62ee8411cda8eefa9404727a8ce205e4903
Kai Hugo Hustoft Endresen <kai.endresen@gmail.com>
2025-08-30 00:03:35 +0200

    Add Qt Message Receiver. (Header only, since we do not want the library to depend on Qt)

---
+++ defs/sepia/comm2/qtmessagereceiver.h
@@ -0,0 +1,42 @@
+#ifndef QT_MESSAGE_RECIEVER_H
+#define QT_MESSAGE_RECEIVER_H
+#include <QObject>
+#include <QSocketNotifier>
+#include <qsocketnotifier.h>
+#include <sepia/comm2/receiver.h>
+#include <sepia/comm2/observerall.h>
+#include <iostream>
+namespace sepia::comm2
+{
+    class QtMessageReceiver : public QObject
+    {
+    public:
+        QtMessageReceiver( ObserverAll* a_observer, QObject* a_object )
+            : QObject( a_object )
+            , m_receiver( a_observer )
+        {
+            m_receiver.init( 0 ); // non-blocking
+
+            int fd = m_receiver.getSocketFileDescriptor();
+
+            m_notifier = new QSocketNotifier( fd, QSocketNotifier::Read, this );
+
+            connect( m_notifier, &QSocketNotifier::activated, this, [ this ]( int )
+            {
+                while( m_receiver.getHaveMessagesAvailable() )
+                {
+                    m_receiver.exec();
+                }
+            } );
+        }
+        QtMessageReceiver() = delete;
+        ~QtMessageReceiver() = default;
+
+    protected:
+    private:
+        sepia::comm2::Receiver m_receiver;
+        QSocketNotifier* m_notifier;
+    };
+}
+
+#endif // QT_MESSAGE_RECEIVER_H
